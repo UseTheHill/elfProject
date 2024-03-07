06a39299750bc247ffbee0e3250f223d89eba8da
usethehill <kaytemcdonough@gmail.com>
2024-03-07 16:17:07 -0500

    Start over commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from 'axios'
-window.axios = axios
 
 const app = createApp(App)
 
