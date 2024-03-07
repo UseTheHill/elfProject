8d17af38fc528446b7537e3e1b3c35853b2c1b98
usethehill <kaytemcdonough@gmail.com>
2024-03-07 17:40:45 -0500

    Add axios dependency and implement user search functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+const searchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+  console.log(searchQuery.value);
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = axios.getSearchResults(
+        `https://api.github.com/users/${searchQuery.value}`
+      );
+      searchResults.value = result;
+      return;
+    }
+    searchResults.value = null;
+  }, 300);
+};
+</script>
 
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Search for a user"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-search-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
+  </main>
 </template>
